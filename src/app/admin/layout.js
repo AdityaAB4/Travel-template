fcd9605dd04ec88bb295f9d69583b350acf7ea22
AdityaAB4 <adityabarde4ab@gmail.com>
2025-04-03 11:13:11 +0530

    travel preview added

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,6 +1,30 @@
 export const metadata = {
   title: "Admin",
-  description: "Admin desc",
+  description: "Admin | Travel and Enjoy",
+  openGraph: {
+    title: "Travel and Enjoy",
+    description: "Book your dream package now!",
+    url: "https://travel-mvp-dev.vercel.app/admin",
+    alternates: {
+      canonical: "https://travel-mvp-dev.vercel.app/admin",
+    },
+    siteName: "Travel and Enjoy",
+    images: [
+      {
+        url: "/travel-preview.png",
+        width: 1200,
+        height: 630,
+        alt: "Twitter ",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Travel and Enjoy",
+    description: "Book your dream package now!",
+    images: ["/travel-preview.png"],
+  },
 };
 
 export default function AdminLayout({ children }) {
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,3 @@
-// import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
-
-// export const metadata = {
-//   title: "Hotel",
-//   description: "Book your dream package now",
-// };
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body className={``}>{children}</body>
-//     </html>
-//   );
-// }
-
-// app/layout.tsx
-
 import "./globals.css";
 import { UserProvider } from "./contexts/UserContext";
 import { Roboto } from "next/font/google";
@@ -25,6 +7,30 @@ const gfont = Roboto({ weight: "400", subsets: ["latin"] });
 export const metadata = {
   title: "Hotel",
   description: "Book your dream package now",
+  openGraph: {
+    title: "Travel and Enjoy",
+    description: "Book your dream package now!",
+    url: "https://travel-mvp-dev.vercel.app/admin",
+    alternates: {
+      canonical: "https://travel-mvp-dev.vercel.app/admin",
+    },
+    siteName: "Travel and Enjoy",
+    images: [
+      {
+        url: "/travel-preview.png",
+        width: 1200,
+        height: 630,
+        alt: "Twitter ",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Travel and Enjoy",
+    description: "Book your dream package now!",
+    images: ["/travel-preview.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
