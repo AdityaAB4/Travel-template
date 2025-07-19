aa7b914ca1e82077d4d264ae6008ea912577e345
AdityaAB4 <adityabarde4ab@gmail.com>
2025-07-19 22:51:31 +0530

    header added to admin packages

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -33,7 +33,13 @@ export default function page() {
 
   const getAllPackages = async () => {
     setLoading(true);
-    const result = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`);
+    const result = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {
+      method: "GET",
+      headers: {
+        "X-Frontend-Domain": window.location.origin,
+        // "X-Frontend-Domain": "localhost",
+      },
+    });
     const packageData = await result.json();
     console.log(packageData, "data");
     setPackages(packageData);
